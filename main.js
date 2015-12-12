89f1424074cf35d73d51bfc84cbde27e914b8254
Reva Arora <revaarora.1991@gmail.com>
2015-12-13 01:22:30 +0530

    api hits n other changes

--- main.js
+++ main.js
@@ -14,7 +14,7 @@
 	    	}
 	    	var template="<div class='box'> <div class='box_side box_img'><img alt='event-logo' src="+image+"></div><div class='box_side box_content'><div class='heading'>"+name;
 	    	template+="</div><div class='heading_1'><span>Solved by:"+solved_by+"|"+rating+"</span><span class='stars'></span></div><div class='heading_2'>"+parent_challenge;
-	    	template+="</div><div class='heading_3'>"+tag+"</div><div class='heading_3'>Link: <a href="+url+">"+url+"</a></div></div></div></div>";
+	    	template+="</div><div class='heading_3'>"+tag+"</div><div class='heading_3'>Link: <a target='_blank' href="+url+">"+url.replace(window.location.origin+'/problem/','')+"</a></div></div></div></div>";
 	    	
 	    	var val =parseFloat(rating),
 	    	    size = Math.max(0, (Math.min(5, val))) * 16,
@@ -25,15 +25,34 @@
 	    },
 
 	    init: function() {
+	    	setInterval(function(){ app.ajax_req_api(); }, 3000);
+	    	
 	        this.ajax_req();
-
+	        this.addActionListener();
+	    },
+	   // http://hackerearth.0x10.info/api/problems?type=json&query=api_hits
+	   	ajax_req_api: function() {
+	        $.ajax({
+	            type: 'GET',
+	            url: 'https://hackerearth.0x10.info/api/problems?type=json&query=api_hits',
+	            success: function(msg) {
+	                app.api = jQuery.parseJSON(msg);
+	                $("#api").text(app.api.api_hits);
+	            },
+	            error: function(request, status, error) {
+	                console.log(error);
+	            }
+	        });
 	    },
 	    ajax_req: function() {
 	        $.ajax({
 	            type: 'GET',
-	            url: 'http://hackerearth.0x10.info/api/problems?type=json&query=list_problems',
+	            url: 'https://hackerearth.0x10.info/api/problems?type=json&query=list_problems',
 	            success: function(msg) {
 	                app.data = jQuery.parseJSON(msg);
+	                if($("#rating").is(':checked')){
+	                	app.sort_by_rating();
+	                }
 	                app.display(app.data);
 	            },
 	            error: function(request, status, error) {
@@ -41,26 +60,28 @@
 	            }
 	        });
 	    },
+	    sort_by_rating :function(){
+	    	app.data.problems.sort(function(a,b){ 
+    			var x = a.rating< b.rating? -1:1; 
+				    return x; 
+				});
+	    },
 	    display: function(data) {
 	        if (data) {
-
 	        	var box='',temp;
 	        	for(var i=0;i<data.problems.length;i++){
 	        			temp = data.problems[i];
 	        			box = this.template_box(temp.image,temp.name,temp.parent_challenge,temp.rating,temp.solved_by,temp.tags,temp.url);
 	        			$(".container").append(box);
 	        		}
-	        	//$(".container").html(boxes);
-
 	            console.log(data);
 	        }
 	        else {
 	        	$(".container").html("NO data received, try refreshing the page");
 	        }
 	    },
-	    display_star:function(rating){
+	    addActionListener: function(){
 	    	
-
 	    }
 	}
 	$.fn.stars =function(rating){
